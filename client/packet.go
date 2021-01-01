02c1b9d1eb24409f80a769ece23e9add615e1b8a
Travis Lane <coderlane@gmail.com>
2021-01-01 18:11:07 -0500

    Move Handshake out of base client
    I'll add this back at a later time. For now, I'm moving it out so it is
    easier to mock out later.

--- client/packet.go
+++ client/packet.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/binary"
 	"io"
 )
@@ -49,63 +48,5 @@ func (pkt *Packet) DecodeBinary(reader io.Reader) error {
 		return err
 	}
 	pkt.Data = data
-	return nil
-}
-
-// Handshake represents the opening handshake with the server
-type Handshake struct {
-	Version VarInt
-	Address VarString
-	Port    uint16
-	State   VarInt
-}
-
-// EncodeBinary encodes the handshake in the binary format for the wire
-func (hnd Handshake) EncodeBinary(writer io.Writer) error {
-	var (
-		err error
-		buf bytes.Buffer
-	)
-	if err = hnd.Version.EncodeBinary(&buf); err != nil {
-		return err
-	}
-	if err = hnd.Address.EncodeBinary(&buf); err != nil {
-		return err
-	}
-	if err = binary.Write(&buf, binary.BigEndian, hnd.Port); err != nil {
-		return err
-	}
-	if err = hnd.State.EncodeBinary(&buf); err != nil {
-		return err
-	}
-	pkt := Packet{
-		ID:   0,
-		Data: buf.Bytes(),
-	}
-	return pkt.EncodeBinary(writer)
-}
-
-// DecodeBinary decodes the handshake from the binary format on the wire
-func (hnd *Handshake) DecodeBinary(reader io.Reader) error {
-	var (
-		err error
-		pkt Packet
-	)
-	if err = pkt.DecodeBinary(reader); err != nil {
-		return err
-	}
-	buf := bytes.NewReader(pkt.Data)
-	if err = hnd.Version.DecodeBinary(buf); err != nil {
-		return err
-	}
-	if err = hnd.Address.DecodeBinary(buf); err != nil {
-		return err
-	}
-	if err = binary.Read(buf, binary.BigEndian, &hnd.Port); err != nil {
-		return err
-	}
-	if err := hnd.State.DecodeBinary(buf); err != nil {
-		return err
-	}
 	return nil
 }
--- client/packet_test.go
+++ client/packet_test.go
@@ -6,14 +6,12 @@ import (
 	"testing"
 )
 
-func TestEncodeDecodeFullHandshake(t *testing.T) {
+func TestEncodeDecodeFullPacket(t *testing.T) {
 	var buf bytes.Buffer
 
-	input := Handshake{
-		Version: -1,
-		Address: "test",
-		Port:    25565,
-		State:   1,
+	input := Packet{
+		ID:   1,
+		Data: []byte("test"),
 	}
 
 	err := input.EncodeBinary(&buf)
@@ -21,7 +19,7 @@ func TestEncodeDecodeFullHandshake(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	var output Handshake
+	var output Packet
 	err = output.DecodeBinary(&buf)
 	if err != nil {
 		t.Fatal(err)
