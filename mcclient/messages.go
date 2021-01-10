a03d27fbf073b86916df4b274715beebdf4f51a5
Travis Lane <coderlane@gmail.com>
2021-01-09 20:14:15 -0500

    Update names and marshaling
    Allow direct marshaling to firebase types and update names to match
    those from configuration files.

--- mcclient/messages.go
+++ mcclient/messages.go
@@ -1,28 +1,33 @@
 package mcclient
 
+// User represents a minecraft user
 type User struct {
-	Name string `json:"name"`
-	ID   string `json:"id"`
+	Name string `json:"name" firebase:"name"`
+	UUID string `json:"id" firebase:"uuid"`
 }
 
+// StatusVersion represents the server version
 type StatusVersion struct {
-	Name     string `json:"name"`
-	Protocol int    `json:"protocol"`
+	Name     string `json:"name" firebase:"name"`
+	Protocol int    `json:"protocol" firebase:"protocol"`
 }
 
+// StatusPlayers represents the players logged in to the server
 type StatusPlayers struct {
-	Max    int    `json:"max"`
-	Online int    `json:"online"`
-	Sample []User `json:"sample"`
+	Max    int    `json:"max" firebase:"max"`
+	Online int    `json:"online" firebase:"online"`
+	Users  []User `json:"sample" firebase:"users"`
 }
 
+// StatusDescription represents the message of the day reported by the server
 type StatusDescription struct {
-	Text string `json:"text"`
+	Text string `json:"text" firebase:"text"`
 }
 
+// StatusResponse is the response to a status request to the server
 type StatusResponse struct {
-	Version     StatusVersion     `json:"version"`
-	Players     StatusPlayers     `json:"players"`
-	Description StatusDescription `json:"description"`
-	Favicon     string            `json:"favicon"`
+	Version     StatusVersion     `json:"version" firebase:"version"`
+	Players     StatusPlayers     `json:"players" firebase:"players"`
+	Description StatusDescription `json:"description" firebase:"description"`
+	Favicon     string            `json:"favicon" firebase:"favicon"`
 }
