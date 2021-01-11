b5e6f3b9158c5b27e4d7b2fce2641983767e2499
Travis Lane <coderlane@gmail.com>
2021-01-10 20:52:23 -0500

    Use "firestore" not "firebase"
    I used the wrong struct tag for firebase objects.

--- mcclient/messages.go
+++ mcclient/messages.go
@@ -2,32 +2,32 @@ package mcclient
 
 // User represents a minecraft user
 type User struct {
-	Name string `json:"name" firebase:"name"`
-	UUID string `json:"id" firebase:"uuid"`
+	Name string `json:"name" firestore:"name"`
+	UUID string `json:"id" firestore:"uuid"`
 }
 
 // StatusVersion represents the server version
 type StatusVersion struct {
-	Name     string `json:"name" firebase:"name"`
-	Protocol int    `json:"protocol" firebase:"protocol"`
+	Name     string `json:"name" firestore:"name"`
+	Protocol int    `json:"protocol" firestore:"protocol"`
 }
 
 // StatusPlayers represents the players logged in to the server
 type StatusPlayers struct {
-	Max    int    `json:"max" firebase:"max"`
-	Online int    `json:"online" firebase:"online"`
-	Users  []User `json:"sample" firebase:"users"`
+	Max    int    `json:"max" firestore:"max"`
+	Online int    `json:"online" firestore:"online"`
+	Users  []User `json:"sample" firestore:"users"`
 }
 
 // StatusDescription represents the message of the day reported by the server
 type StatusDescription struct {
-	Text string `json:"text" firebase:"text"`
+	Text string `json:"text" firestore:"text"`
 }
 
 // StatusResponse is the response to a status request to the server
 type StatusResponse struct {
-	Version     StatusVersion     `json:"version" firebase:"version"`
-	Players     StatusPlayers     `json:"players" firebase:"players"`
-	Description StatusDescription `json:"description" firebase:"description"`
-	Favicon     string            `json:"favicon" firebase:"favicon"`
+	Version     StatusVersion     `json:"version" firestore:"version"`
+	Players     StatusPlayers     `json:"players" firestore:"players"`
+	Description StatusDescription `json:"description" firestore:"description"`
+	Favicon     string            `json:"favicon" firestore:"favicon"`
 }
